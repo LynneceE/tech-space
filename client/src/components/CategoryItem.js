b75dcd842694d2819b85eeb7aea177834e60cc86
brenhamp <bpozzi@trinity.edu>
2022-04-06 00:42:42 -0500

    added server folder and moved app files to client folder

---
+++ client/src/components/CategoryItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CategoryItem = ({item}) => {
+    return (
+        <div>
+            Hello world
+        </div>
+    )
+}
+
+export default CategoryItem;
---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Categories from '../components/Categories';
+
+const Homepage = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Categories/>
+        </div>
+    )
+}
+
+export default Homepage
