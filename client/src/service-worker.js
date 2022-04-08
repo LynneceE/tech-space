09dda83e170b08c7f2c2f08d48290e98be971342
bhari eutsey <bharilynn@outlook.com>
2022-04-08 11:44:09 -0500

    resolvers

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -1,66 +1,66 @@
-const APP_PREFIX = 'TechSpace-';     
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION
-const FILES_TO_CACHE = [
-  "./public/index.html",
-  "./src/App.css",
-  "./src/App.js",
-  "./src/index.js",
-  "./src/index.css",
-  "./src/components/Categories.js",
-  "./src/components/CategoriesItem.js",
-  "./src/components/Navbar.js",
-  "./src/pages/Homepage.js",
-];
+// const APP_PREFIX = 'TechSpace-';     
+// const VERSION = 'version_01';
+// const CACHE_NAME = APP_PREFIX + VERSION
+// const FILES_TO_CACHE = [
+//   "./public/index.html",
+//   "./src/App.css",
+//   "./src/App.js",
+//   "./src/index.js",
+//   "./src/index.css",
+//   "./src/components/Categories.js",
+//   "./src/components/CategoriesItem.js",
+//   "./src/components/Navbar.js",
+//   "./src/pages/Homepage.js",
+// ];
 
 
 
-self.addEventListener('fetch', function (e) { // Respond with cached resources
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-      caches.match(e.request).then(function (request) {
-        if (request) { // if cache is available, respond with cache
-          console.log('responding with cache : ' + e.request.url)
-          return request
-        } else {       // if there are no cache, try fetching request
-          console.log('file is not cached, fetching : ' + e.request.url)
-          return fetch(e.request)
-        }
+// self.addEventListener('fetch', function (e) { // Respond with cached resources
+//     console.log('fetch request : ' + e.request.url)
+//     e.respondWith(
+//       caches.match(e.request).then(function (request) {
+//         if (request) { // if cache is available, respond with cache
+//           console.log('responding with cache : ' + e.request.url)
+//           return request
+//         } else {       // if there are no cache, try fetching request
+//           console.log('file is not cached, fetching : ' + e.request.url)
+//           return fetch(e.request)
+//         }
   
         // You can omit if/else for console.log & put one line below like this too.
         // return request || fetch(e.request)
-      })
-    )
-})
+//       })
+//     )
+// })
   
 
-self.addEventListener('install', function (e) { // Cache resources
-    e.waitUntil(
-      caches.open(CACHE_NAME).then(function (cache) {
-        console.log('installing cache : ' + CACHE_NAME)
-        return cache.addAll(FILES_TO_CACHE)
-      })
-    )
-})
+// self.addEventListener('install', function (e) { // Cache resources
+//     e.waitUntil(
+//       caches.open(CACHE_NAME).then(function (cache) {
+//         console.log('installing cache : ' + CACHE_NAME)
+//         return cache.addAll(FILES_TO_CACHE)
+//       })
+//     )
+// })
   
 
-self.addEventListener('activate', function (e) { // Delete outdated caches
-    e.waitUntil(
-      caches.keys().then(function (keyList) {
-        // `keyList` contains all cache names under your username.github.io
-        // filter out ones that has this app prefix to create keeplist
-        let cacheKeeplist = keyList.filter(function (key) {
-          return key.indexOf(APP_PREFIX);
-        })
-        // add current cache name to keeplist
-        cacheKeeplist.push(CACHE_NAME);
+// self.addEventListener('activate', function (e) { // Delete outdated caches
+//     e.waitUntil(
+//       caches.keys().then(function (keyList) {
+//         // `keyList` contains all cache names under your username.github.io
+//         // filter out ones that has this app prefix to create keeplist
+//         let cacheKeeplist = keyList.filter(function (key) {
+//           return key.indexOf(APP_PREFIX);
+//         })
+//         // add current cache name to keeplist
+//         cacheKeeplist.push(CACHE_NAME);
   
-        return Promise.all(keyList.map(function (key, i) {
-          if (cacheKeeplist.indexOf(key) === -1) {
-            console.log('deleting cache : ' + keyList[i] );
-            return caches.delete(keyList[i]);
-          }
-        }));
-      })
-    );
-});
+//         return Promise.all(keyList.map(function (key, i) {
+//           if (cacheKeeplist.indexOf(key) === -1) {
+//             console.log('deleting cache : ' + keyList[i] );
+//             return caches.delete(keyList[i]);
+//           }
+//         }));
+//       })
+//     );
+// });
