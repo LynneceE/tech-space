fef4e82edf46e8e8ea84fd9e8a22d2f98566da07
brenhamp <bpozzi@trinity.edu>
2022-04-09 13:26:58 -0500

    fixed graphql

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
+const  {
+  ApolloServerPluginLandingPageGraphQLPlayground
+} = require('apollo-server-core');
 
 const {typeDefs, resolvers} = require('./schemas');
 const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const app = express();
 
 const startServer = async () => {
@@ -14,6 +17,9 @@ const startServer = async () => {
     typeDefs,
     resolvers,
     context: authMiddleware,
+    plugins: [
+      ApolloServerPluginLandingPageGraphQLPlayground(),
+    ],
   });
   await server.start();
   server.applyMiddleware({ app });
@@ -26,13 +32,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 db.once('open', () => {
   app.listen(PORT, () => {
