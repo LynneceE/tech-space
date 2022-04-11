0bf2ccb0083f06499fe70315dc887798500868a8
bhari eutsey <bharilynn@outlook.com>
2022-04-10 21:39:16 -0500

    refactored resolvers, adjustments to service worker to fix npm start/run issues (during office hours)

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,71 +3,76 @@ const { User, Product, Category } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-    Query: {
-        categories: async () => {
-            return await Category.find();
-        },
-        products: async (parent, { category, name }) => {
-            const params = {};
+     Query: {
+         categories: async () => {
+             return await Category;
+         },
+         products: async (parent, { category, name }) => {
+             const params = {};
   
-            if (category) {
-                params.category = category;
-            }
+             if (category) {
+                 params.category = category;
+             }
   
-            if (name) {
-                params.name = name;
-            }
+             if (name) {
+                 params.name = name;
+             }
 
-            return await Product.find(params).populate('category');
-        },
-        product: async (parent, { _id }) => {
-            return await Product.findById(_id).populate('category');
-          },
-          user: async (parent, args, context) => {
-            if (context.user) {
-              const user = await User.findById(context.user._id);
+             return await Product;
+         },
+          // product: async (parent, args, context) => { 
+          //     return await Product;
+          //  }, 
+           user: async (parent, args, context) => {
+             if (context.user) {
+               const user = await User;
       
-              return user;
-            }
+               return user;
+             }
 
             throw new AuthenticationError('Not logged in');
-          },
+           },
 
-          Mutation: {
-            addUser: async (parent, args) => {
-              const user = await User.create(args);
-              const token = signToken(user);
-        
-              return { token, user };
-            },
+    },
 
-            updateUser: async (parent, args, context) => {
-                if (context.user) {
-                  return await User.findByIdAndUpdate(context.user._id, args, { new: true });
-                }
-          
-                throw new AuthenticationError('Not logged in');
-              },
 
-              login: async (parent, { email, password }) => {
-                const user = await User.findOne({ email });
-          
-                if (!user) {
-                  throw new AuthenticationError('Incorrect credentials');
-                }
-          
-                const correctPw = await user.isCorrectPassword(password);
-          
-                if (!correctPw) {
-                  throw new AuthenticationError('Incorrect credentials');
-                }
-          
-                const token = signToken(user);
-          
-                return { token, user };
-              }
-            }  
-         },
-    };  
+    Mutation: {
+      addUser: async (parent, args) => {
+        const user = await User.create(args);
+        const token = signToken(user);
+  
+        return { token, user };
+      },
+
+      
+      updateUser: async (parent, args, context) => {
+        if (context.user) {
+          return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+        }
+  
+        throw new AuthenticationError('Not logged in');
+      },
+
+
+
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+  
+        if (!user) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+  
+        const correctPw = await user.isCorrectPassword(password);
+  
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+  
+        const token = signToken(user);
+        return { token, user };
+      },
+      
+    }
+};  
 
 module.exports = resolvers;
