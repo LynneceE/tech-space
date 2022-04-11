3d9d8a16bc31123be1d0d6c3ebf4970cc1432b03
mlbarre <mlbarrera1538@gmail.com>
2022-04-11 17:34:30 -0500

    fixing imports on App.js and pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@
 // import Cart from "./pages/Cart";
  import Homepage from './pages/Homepage';
 import Login from "./pages/Login";
-import Product from "./pages/Product";
+import oneProduct from "./pages/singleProduct";
 import ProductList from "./components/ProductsList";
 import Register from "./pages/Register";
+import None from './pages/None';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -20,16 +21,20 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router>
+    <Router>
         <div className="flex-column justify-flex-start min-100-vh">
+          <Switch>
           {/* <Header /> */}
           <div className="container">
             <Route exact path="/" component={Homepage} />
             <Route exact path="/" component={ProductList} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={Register} />
-            <Route exact path="/product" component={Product} />
+            <Route exact path="/product/:id" component={oneProduct} />
+
+            <Route component={None} />
           </div>
+          </Switch>
           {/* <Footer /> */}
         </div>
       </Router>
